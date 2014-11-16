9932ade4da8c7bcd53861fd5d36929999d13f2ee
Oscar <osky.magodeoz@gmail.com>
2014-11-16 22:29:36 +0100

    tasques repartides + diagrama dependencies

--- Obres/src/Trajecte.java
+++ Obres/src/Trajecte.java
@@ -6,5 +6,5 @@
 public class Trajecte {
 	private LlistaTrams llistaTrams;
 	private String nomTrajecte;
-	private Conductor conductor; //atribut extra cada conductor ja coneix els seus trajectes
+	//private Conductor conductor; //atribut extra cada conductor ja coneix els seus trajectes no tocar de moment
 }
--- Obres/src/Tram.java
+++ Obres/src/Tram.java
@@ -4,10 +4,10 @@
  *
  */
 public class Tram {
-	private Via via; //atribut extra perque cada via ja coneix els seus trams
+	//private Via via; //atribut extra perque cada via ja coneix els seus trams NO TOCAR ATRIBS EXTRA DE MOMENT
 	private int nTram;
 	private String poble1;
 	private String poble2;
-	private LlistaTrajectes llistaTrajectes; //atribut extra perque cada trajecte ja coneix els seus trams
-	private Incidencia incidencia; //atribut extra perque cada incidencia ja coneix el seu tram
+	//private LlistaTrajectes llistaTrajectes; //atribut extra perque cada trajecte ja coneix els seus trams
+	//private Incidencia incidencia; //atribut extra perque cada incidencia ja coneix el seu tram
 }
