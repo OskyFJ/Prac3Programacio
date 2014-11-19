267d156a34971f1c26ec55f621f2e8a02c10540d
Oriol Garcia Gomez <oriol.garciag@estudiants.urv.cat>
2014-11-19 17:03:12 +0100

    Tram (falta comprovar que funcioni) | Oriol García

--- Obres/src/Tram.java
+++ Obres/src/Tram.java
@@ -10,4 +10,38 @@ public class Tram {
 	private String poble2;
 	//private LlistaTrajectes llistaTrajectes; //atribut extra perque cada trajecte ja coneix els seus trams
 	//private Incidencia incidencia; //atribut extra perque cada incidencia ja coneix el seu tram
+	
+	public Tram (int nTram, String poble1, String poble2){
+		this.nTram=nTram;
+		this.poble1=poble1;
+		this.poble2=poble2;		
+	}
+
+	public int getnTram() {
+		return nTram;
+	}
+	
+	public String getPoblacions() {
+		return ("Aquest tram uneix les poblacions "+poble1+" i "+poble2);
+	}
+	
+	public boolean correlative(Tram t2){
+		boolean areCorrelative=false;
+		if(poble1.equals(t2.poble1))
+			areCorrelative=true;
+		if(poble1.equals(t2.poble2))
+			areCorrelative=true;
+		if(poble2.equals(t2.poble1))
+			areCorrelative=true;
+		if(poble2.equals(t2.poble2))
+			areCorrelative=true;
+		return areCorrelative;	
+	}
+	
+	public String toString() {
+		return "Tram [nTram=" + nTram + ", poble1=" + poble1 + ", poble2="
+				+ poble2 + "]";
+	}
+   
+	
 }
